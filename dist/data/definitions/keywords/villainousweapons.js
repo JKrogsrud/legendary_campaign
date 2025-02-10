a8cbf81f27c6dea6e1bc26ed1baebccd8e831359
Jared <jkrogsrud@gmail.com>
2025-02-10 13:25:25 -0500

    test

--- dist/data/definitions/keywords/villainousweapons.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.villainousweapons = void 0;
-var villainousweapons_1 = require("../rules/villainousweapons");
-Object.defineProperty(exports, "villainousweapons", { enumerable: true, get: function () { return villainousweapons_1.villainousweapons; } });
